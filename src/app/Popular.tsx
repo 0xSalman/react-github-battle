aa122dcdde4dc622c49979655b5d6ae7f88c83cb
Salman Ahmad <m.salman86@gmail.com>
2017-05-30 17:22:15 -0400

    Fetch popular repos from github. Added component to display them

--- src/app/Popular.tsx
+++ src/app/Popular.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import {SelectLanguage} from "./SelectLanguage";
+import {api} from "./common/api";
+import {RepoGrid} from "./RepoGrid";
 
 interface LanguageSate {
-  selectedLanguage: string
+  selectedLanguage: string,
+  repos: Array<any> | null
 }
 
 export class Popular extends React.Component<any, LanguageSate> {
@@ -10,13 +13,25 @@ export class Popular extends React.Component<any, LanguageSate> {
   constructor(props: any, context: LanguageSate) {
     super(props, context);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     }
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
   }
 
   updateLanguage(lang: string) {
     this.setState({
-      selectedLanguage: lang
+      selectedLanguage: lang,
+      repos: null
+    });
+
+    api.fetchPopularRepos(lang).then(repos => {
+      this.setState({
+        repos: repos
+      })
     });
   }
 
@@ -26,6 +41,9 @@ export class Popular extends React.Component<any, LanguageSate> {
       <div>
         <SelectLanguage selectedLanguage={this.state.selectedLanguage}
                         onSelect={this.updateLanguage.bind(this)}/>
+        {!this.state.repos
+          ? <p>LOADING...</p>
+          : <RepoGrid repos={this.state.repos}/>}
       </div>
     );
   }
---
+++ src/app/common/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const api = {
+
+  fetchPopularRepos: (lang: string): Promise<Array<any>> => {
+
+    var encodedURI = (window as any).encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + lang + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodedURI)
+      .then((response) => {
+        return response.data.items;
+      });
+  }
+};
